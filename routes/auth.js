aea932072e8014b74c63445727db2dd93b7a7cfd
arungith30 <arunks002info@gmail.com>
2023-05-20 09:10:35 +0530

    auth register

--- routes/auth.js
+++ routes/auth.js
@@ -14,9 +14,9 @@ router.post("/register", async (req, res) => {
     });
 
     const user = await newUser.save();
-    res.status(200).json(user);
+   return res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err);
+    return res.status(500).json(err);
   }
 });
 
